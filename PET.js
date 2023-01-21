20f33edff7241cbd372c096118de82a0dd998363
Carlos Apolaya <temis_it@hotmail.com>
2023-01-20 19:56:57 -0500

    ne

--- PET.js
+++ PET.js
@@ -142,7 +142,7 @@ let questions = [
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
 let count = 0;
-const questionTime = 120; // 10s
+const questionTime = 240; // 10s
 const gaugeWidth = 150; // 150px
 const gaugeUnit = gaugeWidth / questionTime;
 let TIMER;
